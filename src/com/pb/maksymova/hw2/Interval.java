c8b56a479427572b99a28ad2ff92aa1ccc5bf7c1
Yana Maksymova <petrenkoya@gmail.com>
2021-10-15 23:39:12 +0300

    init commit

--- src/com/pb/maksymova/hw2/Interval.java
+++ src/com/pb/maksymova/hw2/Interval.java
@@ -20,6 +20,8 @@ public class Interval {
             System.out.println("Число входит в диапапзон [36 - 50]");
         } else if (x>=51 && x <100) {
             System.out.println("Число входит в диапапзон [51 - 100]");
+        } else {
+            System.out.println("Число не входит ни в один имеющийся диапапзон");
         }
     }
 }
